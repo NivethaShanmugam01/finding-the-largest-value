0ea29c2f6da9057b1fde4de5114f58eaa04a9b45
NivethaShanmugam01 <135867458+NivethaShanmugam01@users.noreply.github.com>
2023-08-26 10:50:08 +0530

    Create largest value.py

---
+++ largest value.py
@@ -0,0 +1,15 @@
+a = int(input("enter the value for a "))
+b = int(input("enter the value for b "))
+c = int(input("enter the value for c "))
+d = int(input("enter the value for d "))
+e = int(input("enter the value for e "))
+largest_value=a
+if b > largest_value:
+  largest_value = b
+if c > largest_value:
+  largest_value = c
+if d > largest_value:
+  largest_value = d
+if e > largest_value:
+  largest_value = e
+print(largest_value)
